c5886e949db53355814abfffba8bc52bb9e8c4bb
Egor Shakhmin <lapter57@gmail.com>
2020-01-13 01:11:11 +0300

    Fixed

--- scripts/audio_downloader.py
+++ scripts/audio_downloader.py
@@ -5,13 +5,13 @@ import os
 import pandas as pd
 from argparse import ArgumentParser
 
-def download_audio(path, start_idx, end_idx, length=None):
-    df = pd.read_csv(args.path,
+def download_audio(path, dest, start_idx, end_idx, length=None):
+    df = pd.read_csv(path,
                      usecols=[0,1,2],
                      names=["youtube_id", "start_time", "end_time"],
                      comment="#")
     try:
-        os.mkdir(args.dest)
+        os.mkdir(dest)
     except OSError:
         pass
     id = 0
@@ -42,4 +42,4 @@ if __name__ == "__main__":
     parser.add_argument("--length", action="store", type=float, dest="length",
                         default=3.0, help="Audio duration(default=3.0)")
     args = parser.parse_args()
-    download_audio(args.dest, args.start_idx, args.end_idx, args.length)
+    download_audio(args.path, args.dest, args.start_idx, args.end_idx, args.length)
--- scripts/init.py
+++ scripts/init.py
@@ -11,13 +11,13 @@ adb.init_dir("../data")
 adb.init_dir("../data/csv")
 adb.init_dir("../data/audio")
 
-wget.download(TRAIN_CSV_URL, '../data/csv/avspeech_train.csv')
-wget.download(TEST_CSV_URL, '../data/csv/avspeech_test.csv')
-wget.download(NOISE_CSV_URL, '../data/csv/noise.csv')
+wget.download(TRAIN_CSV_URL, "../data/csv/avspeech_train.csv")
+wget.download(TEST_CSV_URL, "../data/csv/avspeech_test.csv")
+wget.download(NOISE_CSV_URL, "../data/csv/noise.csv")
 
-ad.download_audio("../data/audio/speakers_train", 0, 1000, 3.0)
-ad.download_audio("../data/audio/speakers_test", 0, 500, 3.0)
-ad.download_audio("../data/audio/noise", 0, 500, 3.0)
+ad.download_audio("../data/csv/avspeech_train.csv", "../data/audio/speakers_train", 0, 1000, 3.0)
+ad.download_audio("../data/csv/avspeech_test.csv", "../data/audio/speakers_test", 0, 500, 3.0)
+ad.download_audio("../data/csv/noise.csv", "../data/audio/noise", 0, 500, 3.0)
 
 adb.build("../data/audio_train", "../data/audio/speakers_train", 
           1000, 2, "../data/audio/noise")
