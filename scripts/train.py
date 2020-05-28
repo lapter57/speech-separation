08f700a0bc84f78bc5a3511ddc74bd402665376d
Lapter57 <lapter57@gmail.com>
2020-05-28 19:51:04 +0000

    Added a way of restarting model

--- scripts/train.py
+++ scripts/train.py
@@ -10,8 +10,11 @@ if __name__ == "__main__":
     parser.add_argument("--config", action="store",
                         dest="config", required=True,
                         help="Path to a config file")
+    parser.add_argument("--chkpt", action="store",
+                        dest="chkpt_file", default=None,
+                        help="Path to a checkpoint file")
     args = parser.parse_args()
     config = Config(args.config)
-    trainer = Trainer(config)
+    trainer = Trainer(config, args.chkpt_file)
     trainer.train()
 
